9f804cdee3d8af4426d180c47f82803061ac6e8f
YasinY <yasin_programmer@hotmail.com>
2015-12-09 20:31:04 +0100

    newest update eoc 2.0 jesus hail mary tupac biggie

--- CronScript/src/org/dreambot/cronscript/internal/Faggot.java
+++ CronScript/src/org/dreambot/cronscript/internal/Faggot.java
@@ -4,4 +4,28 @@ package org.dreambot.cronscript.internal;
  * Created by Yasin on 09/12/2015.
  */
 public class Faggot {
+
+    private String name;
+    private boolean penis;
+    private double penisSize;
+    private int balls;
+
+
+    public Faggot() {
+
+    }
+
+    public void checkForArno() {
+        if(name.equalsIgnoreCase("arno")) {
+            penis = false;
+            balls = -1;
+        }
+        if(name.equalsIgnoreCase("steroidz arno")) {
+            penis = true;
+            balls = 2;
+            penisSize = 1.9;
+
+        }
+    }
+
 }
